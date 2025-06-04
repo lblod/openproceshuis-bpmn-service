e6e52e74fdeb1ecbac742041771c22a4f27062ac
Andreo Van Onckelen <andreo141mlg@gmail.com>
2025-06-04 03:21:44 +0200

    Errormessages in dutch for ease of use in oph frontend

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const HEADER_MU_SESSION_ID = "mu-session-id";
 const JOB_GRAPH = "http://mu.semte.ch/graphs/bpmn-job";
 const JOB_OPERATION =
   "http://redpencil.data.gift/id/jobs/concept/JobOperation/BpmnToRdf";
-import { HttpError } from "./utils/http-error.js";
+import { HttpError, UploadError } from "./utils/http-error.js";
 
 app.use(
   bodyParser.json({
@@ -32,23 +32,36 @@ app.post("/", async (req, res, next) => {
   try {
     const sessionUri = req.get(HEADER_MU_SESSION_ID);
     if (!sessionUri) {
-      throw new HttpError("Session ID header not found.", 401);
+      throw new HttpError(
+        "Session ID header werd niet gevonden.",
+        401,
+        "The session ID is not found. Are you sure you are logged in?"
+      );
     }
     const groupUriQuery = generateGroupUriSelectQuery(sessionUri);
     const groupUriResult = await querySudo(groupUriQuery);
     const groupUri = groupUriResult.results.bindings[0]?.groupUri?.value;
     if (!groupUri) {
-      throw new HttpError("User not affiliated with any organization.", 401);
+      throw new HttpError(
+        "Gebruiker maakt geen deel uit van een organisatie.",
+        401
+      );
     }
     const virtualFileUuid = req.query.id;
     if (!virtualFileUuid) {
-      throw new HttpError("Missing file id in query parameters.", 400);
+      throw new HttpError(
+        "Bestand id ontbrak tijdens het uploaden van het bpmn bestand.",
+        400
+      );
     }
     const fileUriQuery = generateFileUriSelectQuery(virtualFileUuid);
     const fileUriResult = await query(fileUriQuery);
     const fileUriBindings = fileUriResult.results.bindings;
     if (fileUriBindings.length === 0) {
-      throw new HttpError("File id was not found!", 404);
+      throw new HttpError(
+        `Bestand id ${virtualFileUuid} werd niet gevonden in onze server.`,
+        404
+      );
     }
     const virtualFileUri = fileUriBindings[0].virtualFileUri.value;
     const physicalFileUri = fileUriBindings[0].physicalFileUri.value;
@@ -61,10 +74,7 @@ app.post("/", async (req, res, next) => {
 
     const filePath = physicalFileUri.replace("share://", STORAGE_FOLDER_PATH);
     if (!existsSync(filePath)) {
-      throw new HttpError(
-        "Could not find file in path. Check if the physical file is available on the server and if this service has the right mountpoint.",
-        500
-      );
+      throw new HttpError("Kan bestand in pad niet vinden.", 500);
     }
 
     runAsyncJob(JOB_GRAPH, JOB_OPERATION, groupUri, virtualFileUri, () =>
@@ -92,7 +102,7 @@ async function extractAndInsertProcessSteps(bpmnFilePath, virtualFileUri) {
 async function translateToRdf(bpmn, virtualFileUri) {
   if (!bpmn || bpmn.trim().length === 0) {
     throw new HttpError(
-      "Invalid content: The provided file does not contain any content.",
+      "Ongeldige inhoud: Het meegeleverde bestand bevat geen inhoud.",
       400
     );
   }
@@ -115,7 +125,7 @@ async function translateToRdf(bpmn, virtualFileUri) {
   );
   if (!triples || triples.trim().length === 0) {
     throw new HttpError(
-      "Invalid content: The provided file does not contain valid content.",
+      "Ongeldige inhoud: Het meegeleverde bestand heeft geen geldige inhoud.",
       400
     );
   }
@@ -164,7 +174,13 @@ async function insertTripleChunks(tripleChunks, maxTriplesPerInsert = 100) {
 const errorHandler = function (err, _req, res, _next) {
   res.status(err.status || 500);
   res.json({
-    errors: [{ title: err.message, description: err.description?.join("\n") }],
+    errors: [
+      {
+        message: err.message,
+        description: err.description,
+        status: err.status,
+      },
+    ],
   });
 };
 
